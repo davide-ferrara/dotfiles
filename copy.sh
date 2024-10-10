a1dac1dba988e6d699256b093d072933cf408be9
davide-ferrara <davide98ferrara@gmail.com>
2024-10-10 14:22:18 +0200

    added rofi menu and new i3 keybinds

--- copy.sh
+++ copy.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-# Directory where the repository's config files are located
-CONFIG_DIR=$(pwd)/config
-
-# Destination directory (~/.config)
-DEST_DIR="$HOME/.config"
-
-# Function to create symlink if it doesn't already exist or overwrite if necessary
-create_symlink() {
-    local src="$1"
-    local dest="$2"
-    
-    if [ -L "$dest" ] || [ -d "$dest" ]; then
-        echo "Removing existing $dest"
-        rm -rf "$dest"
-    fi
-
-    ln -s "$src" "$dest"
-    echo "Created symlink: $src -> $dest"
-}
-
-# Create symbolic links for each configuration file/folder
-create_symlink "$CONFIG_DIR/i3" "$DEST_DIR/i3"
-create_symlink "$CONFIG_DIR/kitty" "$DEST_DIR/kitty"
-create_symlink "$CONFIG_DIR/nitrogen" "$DEST_DIR/nitrogen"
-create_symlink "$CONFIG_DIR/nvim" "$DEST_DIR/nvim"
-create_symlink "$CONFIG_DIR/polybar" "$DEST_DIR/polybar"
+apt-get install rofi;
+ln -s "rofi/cyano.rasi" "/usr/share/rofi/themes/"
 
 echo "Symbolic links successfully created."
 
